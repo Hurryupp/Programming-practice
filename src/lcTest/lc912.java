1c7e874b049b1eff2528765e34fc03bdef97b262
Hurry <569030065@qq.com>
2022-03-21 22:46:18 +0800

    Update

--- src/lcTest/lc912.java
+++ src/lcTest/lc912.java
@@ -12,45 +12,17 @@ import java.util.Arrays;
 public class lc912 {
     @Test
     public void test(){
-        int[] nums = {5, 2, 3, 1};
-        int[] array = sortArray(nums);
-        System.out.println(Arrays.toString(array));
-    }
-    public int[] sortArray(int[] nums) {
-        temp = new int[nums.length];
-        Sort(nums,0,nums.length-1);
-        return nums;
+        int[] nums = {5, 3, 1, 4, 2, 7, 6};
+        int[] ints = Sort(nums);
+        System.out.println(Arrays.toString(ints));
     }
     int[] temp;
-    public void Sort(int[] nums,int low,int high){
-        if (low >= high) {
-            return;
-        }
-        int mid = (low+high)/2;
-        Sort(nums,low,mid); //左边排序
-        Sort(nums,mid+1,high);  //右边排序
-        Merge(nums,low,mid,high);
+    public int[] Sort(int[] nums){
+        temp = new int[nums.length];
+        //递归调用左子树
+        Sort(nums)
     }
-    //合并数组,并将数组排序
-    public void Merge(int[] nums,int low,int mid,int high){
-        for (int i = low; i <= high; i++) {
-            temp[i] = nums[i];
-        }
-        //双指针合并
-        int i = low;
-        int j = mid+1;
-        for (int k = low; k <= high; k++) {
-            if (i == mid+1){
-                nums[k] = temp[j++];
-            } else if (j == high) {
-                nums[k] = temp[i++];
-            }else if (temp[i]>temp[j]){
-                nums[k] = nums[j++];
-            }
-            else{
-                nums[k] = nums[i++];
-            }
-        }
-    }
+    public void  Merge(int nums){
 
+    }
 }
